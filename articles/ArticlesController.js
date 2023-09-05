f38cd11bfa39a202808613b2aa00119aba50e46e
Leonardo Rosa <leonardo.rosa@mobicare.com.br>
2023-09-05 18:06:30 -0300

    articles implementation.

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,53 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugfy = require("slugify");
+const { default: slugify } = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
-	res.send("ARTIGOS");
+	Article.findAll({
+		include: [{ model: Category }]
+	}).then(articles => {
+		res.render("admin/articles/index", { articles: articles });
+	});
 });
 
 router.get("/admin/articles/new", (req, res) => {
-	res.render("admin/articles/new");
+	Category.findAll().then(categories => {
+		res.render("admin/articles/new", { categories: categories });
+	});
+});
+
+router.post("/articles/save", (req, res) => {
+	const title = req.body.title;
+	const body = req.body.article;
+	const category = req.body.category;
+
+	Article.create({
+		title: title,
+		body: body,
+		slug: slugify(title),
+		categoryId: category
+	}).then(() => {
+		res.redirect("/admin/articles");
+	})
+})
+
+router.post("/admin/articles/delete", (req, res) => {
+	let id = req.body.id;
+
+	if (id != undefined) {
+		if (!isNaN(id)) {
+			Article.destroy({ where: { id: id } }).then(() => {
+				res.redirect("/admin/articles");
+			});
+		} else {
+			res.redirect("/admin/articles");
+		}
+	} else {
+		res.redirect("/admin/articles");
+	}
 });
 
 module.exports = router;
