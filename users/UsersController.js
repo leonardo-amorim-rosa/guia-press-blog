b75bef863532fde167ab832189aace17fb24e15c
Leonardo Rosa <leonardo.rosa@akross.com.br>
2024-01-30 10:50:43 -0300

    create users

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require('bcryptjs');
 
-router.get("/users/create", (req, res) => {
-    res.send("Users form");
+router.get("/admin/users", (req, res) => {
+    res.send("List users");
+});
+
+router.get("/admin/users/create", (req, res) => {
+    res.render("admin/users/create");
+});
+
+router.post("/admin/users/create", (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password);
+
+    User.create({ email: email, password: hash }).then(() => {
+        res.redirect("/");
+    }).catch(err => {
+        res.redirect("/");
+    });
 });
 
 module.exports = router;
